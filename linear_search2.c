2ed20455b3169505da236bd097b5479702ad009b
pratz0499 <36686740+pratz0499@users.noreply.github.com>
2018-08-14 19:18:37 +0530

    Update linear_search2.c

--- linear_search2.c
+++ linear_search2.c
@@ -1,13 +1,5 @@
 #include<stdio.h>
-
-#define MAXM 10
-#define MAXN 10
-struct Two_D_array {
-   int m,n;
-   int a[MAXM][MAXN];
-};
-
-int lsearch(int m,int n,int a[m][n],int key);
+#include<lserach>
 
 
 struct Two_D_array inputarray()
@@ -32,56 +24,5 @@ int lsearch( struct Two_D_array t, int key) {
 }
 
 
-int main()
-{
- struct Two_D_array t;
- t = inputarray();
- int key;
- /* write get key */
- int pos = lsearch(t,key);
- ...
-}
+
  
- return 0;
-}
-
-
-void main()
-{
- int a[10][10],i,j,n,m,key,position;
- printf("Enter the size of an array: ");
- scanf("%d%d",&n,&m);
- printf("Enter the elements of n:");
- for(i=0;i<n;i++)
- {
-
-  for(j=0;j<m;j++)
- {
- scanf("%d",&a[i][j]);
- }
-}
- printf("Enter the key element:");
- scanf("%d",&key);
-
-lsearch(a[n][m],key);
-}
-
-int lsearch(int a[p][q],int key)
-{
-int i,j;
-  for(i=0;i<p;i++)
-  {
-    for(j=0;j<q;j++)
-     {
-      if (a[i][j]==key)
-      {
-       printf("The element is found ");
-      }
-     else
-       {
-        printf("The element is not found");
-       }
-    }
- }
-return 0;
-}
