0ff514f455efd93fe58473ae7ef64d72b3187e03
pratz0499 <prarthanaparu04@gmail.com>
2019-07-08 12:22:50 +0530

    assignment of integers to romans

---
+++ int_roman.py
@@ -0,0 +1,16 @@
+class conversion:
+    def __init__(self,integer):
+        pass
+    def int_rom(self):
+        while self.integer<=10:
+            a=str(self.integer)
+            b=len(a)
+            1=i
+            5=v
+            10=x
+            
+
+
+
+
+
